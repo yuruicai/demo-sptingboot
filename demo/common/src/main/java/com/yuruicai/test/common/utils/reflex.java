345fefa4766c6c60f3b67b96ef7c46ada2cd6238
yuruicai <yuruicai@sinochem.com>
2019-04-30 15:54:13 +0800

    test

---
+++ demo/common/src/main/java/com/yuruicai/test/common/utils/reflex.java
@@ -0,0 +1,11 @@
+package com.yuruicai.test.common.utils;
+/**
+ * @Description:
+ * @Author: yrc
+ * @CreateDate: 2019/4/30 15:19
+ * @UpdateDate: 2019/4/30 15:19
+ */
+
+
+public class reflex {
+}
---
+++ demo/restful/src/main/java/com/yuruicai/test/resfull/ResfullApplication.java
@@ -0,0 +1,13 @@
+package com.yuruicai.test.resfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResfullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResfullApplication.class, args);
+    }
+
+}
