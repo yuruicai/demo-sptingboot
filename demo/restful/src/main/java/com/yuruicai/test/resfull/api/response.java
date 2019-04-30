4f00fada893995e4bf2bdf169f29582a9f777dbf
yuruicai <yuruicai@sinochem.com>
2019-04-30 15:55:47 +0800

    test

---
+++ demo/restful/src/main/java/com/yuruicai/test/resfull/api/response.java
@@ -0,0 +1,11 @@
+package com.yuruicai.test.resfull.api;
+/**
+ * @Description:
+ * @Author: yrc
+ * @CreateDate: 2019/4/30 15:43
+ * @UpdateDate: 2019/4/30 15:43
+ */
+
+
+public class response {
+}
